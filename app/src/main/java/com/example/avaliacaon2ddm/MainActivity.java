5011cfbc27cfbb0f1d13b0c0596ffb1fc8ae0975
wrc7 <47692629+wrc7@users.noreply.github.com>
2020-11-09 21:26:40 -0300

    code

--- app/src/main/java/com/example/avaliacaon2ddm/MainActivity.java
+++ app/src/main/java/com/example/avaliacaon2ddm/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.avaliacaon2ddm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tLogin = (TextView) findViewById(R.id.tLogin);
+                TextView tSenha = (TextView) findViewById(R.id.tSenha);
+                String login = tLogin.getText().toString();
+                String senha = tSenha.getText().toString();
+
+                if(login.equals("willian") && senha.equals("1234")){
+                    alert("Login efetuado com sucesso!");
+                }else {
+                    alert("Falha -> Usuário ou senha incorreto!");
+                }
+            }
+        });
+    }
+
+    private void alert(String s){
+        Toast.makeText(this,s,Toast.LENGTH_LONG).show();
     }
 }
